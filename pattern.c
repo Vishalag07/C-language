d4e6d1a32b301031bca46780ccdd56ba7f58c8c0
Vishalag07 <148445998+Vishalag07@users.noreply.github.com>
2024-09-27 15:31:54 +0530

    Update pattern.c

--- pattern.c
+++ pattern.c
@@ -1,13 +1,18 @@
-//code to print right half pyramid
-#include<stdio.h>
-int main(){
-    int i,j,rows;
-    printf("enter no. of rows :");
-    scanf("%d",&rows);
-    for(i=0;i<=rows;i++){
-        for(j=0;j<=i;j++){
-            printf("%d",j+1);
-        }
-        printf("\n");
-    }
+// C program to print right half pyramid pattern of number 
+#include <stdio.h> 
+
+int main() 
+{ 
+	int rows = 5; 
+
+	// first loop for printing rows 
+	for (int i = 0; i < rows; i++) { 
+
+		// second loop for printing number in each rows 
+		for (int j = 0; j <= i; j++) { 
+			printf("%d ", j + 1); 
+		} 
+		printf("\n"); 
+	} 
+	return 0; 
 }
