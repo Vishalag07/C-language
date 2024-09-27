856829e20f58b13259e0a02d64a0d39c6e7faba1
Vishalag07 <148445998+Vishalag07@users.noreply.github.com>
2024-09-27 15:20:03 +0530

    Update check_armstrong.c

--- check_armstrong.c
+++ check_armstrong.c
@@ -1,24 +1,18 @@
 //code to check a number is armstrong number or not
 #include<stdio.h>
-#include<math.h>
 void main(){
-    int n,no,s=0,i=0,r;
+    int n,i,num,s=0,r;
     printf("enter number :");
     scanf("%d",&n);
-    no=n;
-    while(no!=0){
-        no=no/10;
-        i++;
+    num=n;
+    for(i=0;num!=0;i++){
+        r=num%10;
+        s=s+(r*r*r);
+        num=num/10;
     }
-    no=n;
-    while(no!=0){
-        r=no%10;
-        s=s+pow(r,i);
-        no=no/10;
-    }
-    if(n==s)
-    printf("ARMSTRONG number");
+    if(s==n)
+    printf("%d is an armstrong number",n);
     else
-    printf("not an ARMSTRONG number");
+    printf("%d is not an armstrong number",n);
 }
 
